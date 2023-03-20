0bcac168759094ddd7d0069003d601956655b885
JARDEL BUDKE <jardel.budke@compasso.com.br>
2023-03-20 17:13:41 -0300

    Added igniteFeed JS

---
+++ src/components/Comment.jsx
@@ -0,0 +1,46 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react';
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComent() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    // setLikeCount(likeCount + 1);
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/jardelbudke.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Jardel Dal Castell Budke</strong>
+              <time title="20 de Março às 08:13h" dateTime="2023-03-20 08:13:00">Cerca de 5h atrás</time>
+            </div>
+            <button onClick={handleDeleteComent} title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
